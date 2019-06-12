5fccba3e265bf3ef38577507d4349ac9cd1bf04c
Matthew Blount <xkapastel@gmail.com>
2019-06-12 12:27:38 -0400

    Trivial Environment type, failing tests.

---
+++ test/normalize.dart
@@ -0,0 +1,37 @@
+// This file is a part of Unitype.
+// Copyright (C) 2019 Matthew Blount
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <https://www.gnu.org/licenses/.
+
+import "package:test/test.dart";
+import "package:unitype/logic.dart" as logic;
+
+void main() {
+  var env = logic.Environment();
+  var src = {
+    "[foo] [bar] a": "bar [foo]",
+    "[foo] [bar] b": "[[foo] bar]",
+    "[foo] [bar] c": "[foo] [bar] [bar]",
+    "[foo] [bar] d": "[foo]",
+  };
+  for (var entry in src.entries) {
+    var program = entry.key;
+    var expected = entry.value;
+    var actual = env(program);
+    test("${program} = ${expected}", () {
+      expect(actual, equals(expected));
+    });
+  }
+}
